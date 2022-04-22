1aa755852da5699e83ddab5c69c18f45fb2bb946
Alejandro Moreno <ale.moreno991@gmail.com>
2022-04-22 10:58:10 -0500

    DDPG algorithm working (needs some tuning still)

--- gym_mujoco/main.py
+++ gym_mujoco/main.py
@@ -5,6 +5,7 @@
 #
 ################################################################################
 import gym_multirotor.envs.mujoco.quadrotor_plus_hover as quad
+from DDPG import DDPG
 
 import numpy as np
 import time 
@@ -13,19 +14,42 @@ import time
 
 env = quad.QuadrotorPlusHoverEnv()
 
-print(env.action_space)
-print(env.observation_space)
-print(env.observation_space.high)
-print(env.observation_space.low)
+agent = DDPG(
+    act_dim=4, 
+    obs_dim=18,
+    lr_actor=0.0001, 
+    lr_q_value=0.001, 
+    gamma=0.8, 
+    tau=0.01, 
+    action_noise_std=0.5
+)
 
-for _ in range(20):
+nepisode = 1000
+iteration = 0
+
+for i_episode in range(nepisode):
     done = False
-    s = env.reset()
+    obs0 = env.reset()
+    ep_rwd = 0
+
     while not done:
         env.render()
-        a = 0.4905 * np.ones(4)
-        s_tilde,r,done,_ = env.step(a) # take a random action
-        s=s_tilde
-    time.sleep(1)
+
+        act = agent.step(obs0)
+
+        obs1, rwd, done, _ = env.step(act)
+
+        agent.memory.store_transition(obs0, act, rwd, obs1, done)
+
+        obs0 = obs1
+        ep_rwd += rwd
+
+        if iteration >= 128 * 3:
+            agent.learn()
+
+        iteration += 1
+
+        
+    print('Ep: %i' % i_episode, "|Ep_r: %i" % ep_rwd)
 
 env.close()
